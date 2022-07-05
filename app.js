863991c36f5b5efd6702d8426b3745fe3bbdbc6e
Saneesh Vly <saneesh.vly@gmail.com>
2022-07-05 17:15:06 +0530

    dsfsd

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('cookie-session')
-
-var indexRouter = require('./routes/nseStocks');
 const config = require('./config/config');
 var app = express();
-let http = require('http');
-
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -20,8 +15,7 @@ mongoose.connect( "mongodb://" + config.getConfig('dbusername') + ":" + config.g
         console.log("db connected")
     }).catch(err => {
         console.log("err occured in db connection", err);
-    })
-// mongoose.set('useCreateIndex', true);
+    });
 mongoose.connection.once('open', function () {
 }).on('error', function (error) {
     console.log('Error in Connection With Mongodb:', error);
@@ -31,12 +25,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-// app.use('/nseStocks', indexRouter);
-
 const index = require('./routes/');
 app.use('', index);
-
-
 app.listen(4000, function (req,res) {
     console.log('Express server listening on port 3000');
 });
